a062e46b8b5997270743eb6a734e5b8a64a39953
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-05-21 11:34:42 -0500

    Update Portero.java

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -23,7 +23,7 @@ public class Portero extends Futbolista{
   }
   @Override
   public String toString(){
-    return "El futbolista "+getNombre()+" tiene "+getEdad()+" y juega de "+getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+ golesRecibidos;
+    return "El futbolista "+getNombre()+" tiene "+getEdad()+", y juega de "+getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+ golesRecibidos;
   }
   
 }
